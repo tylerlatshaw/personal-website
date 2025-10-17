bad8c6a4417ed8f55138efa8e301cbf254f3e990
Tyler Latshaw <tylerlatshaw@gmail.com>
2025-10-16 21:39:00 -0400

    Updated Turnstile to always show

--- src/components/global-components/footer-contact.tsx
+++ src/components/global-components/footer-contact.tsx
@@ -125,7 +125,7 @@ export default function FormFooterContact() {
                     onSuccess={(e) => setTurnstileToken(e)}
                     options={{
                         theme: "dark",
-                        appearance: "interaction-only",
+                        appearance: "always",
                         size: "normal"
                     }}
                 />
